4839b2f83cf122c66db1b4c075957a672439bd8b
wholesomedev <mk@hollowverse.com>
2018-04-22 10:40:55 -0700

    Fix: remove clownCallback in TS feature

--- src/cli/clown.ts
+++ src/cli/clown.ts
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 import program from 'commander';
 
-// tslint:disable-next-line:no-var-requires no-require-imports
-require('ts-node').register({
-  transpileOnly: true,
-  'skip-project': true,
-});
-
 program
   .command('extend', 'Tells clown to scaffold/extend', { isDefault: true })
   .command(
