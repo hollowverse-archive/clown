c27b492e85822e59df7d45c5186d98ab05c87993
wholesomedev <mk@hollowverse.com>
2018-04-21 21:33:50 -0700

    Other: simplify normalizeBasename

--- src/normalizeBasename.ts
+++ src/normalizeBasename.ts
@@ -3,12 +3,7 @@ import path from 'path';
 export function normalizeBasename(filePath: string) {
   const basename = path.basename(filePath);
 
-  if (basename.startsWith('c__')) {
-    const dirname = path.dirname(filePath);
-    const normalizedBasename = path.basename(filePath).slice(3);
-
-    return path.join(dirname, normalizedBasename);
-  }
-
-  return filePath;
+  return basename.startsWith('c__')
+    ? path.join(path.dirname(filePath), basename.slice(3))
+    : filePath;
 }
