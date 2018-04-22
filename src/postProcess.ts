20d1d161427118e0273da83f1c473755eac13118
wholesomedev <mk@hollowverse.com>
2018-04-21 21:39:44 -0700

    Other: simplify postProcess

--- src/postProcess.ts
+++ src/postProcess.ts
@@ -8,23 +8,17 @@ export function postProcess(
   extensionPathAndSourceFiles: ExtensionPathAndSourceFiles,
 ): FileContents {
   const [extensionPath, sourceFiles] = extensionPathAndSourceFiles;
+  const findFile = (filename: string) =>
+    _.find(sourceFiles, sourceFile => path.basename(sourceFile) === filename);
 
   const clownCallbackRelativePath =
-    _.find(
-      sourceFiles,
-      sourceFile => path.basename(sourceFile) === 'clownCallback.ts',
-    ) ||
-    _.find(
-      sourceFiles,
-      sourceFile => path.basename(sourceFile) === 'clownCallback.js',
-    );
+    findFile('clownCallback.ts') || findFile('clownCallback.js');
 
   if (!clownCallbackRelativePath) {
     return fileContents;
   }
 
   const clownCallbackPath = path.join(extensionPath, clownCallbackRelativePath);
-
   const clownFilesystem = new ClownFilesystem(fileContents);
 
   // tslint:disable:next non-literal-require no-parameter-reassignment
